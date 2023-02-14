e7afd70da27ae4e1a40892682674efaf45a0e92d
Théo Monnom <theo.monnom@outlook.com>
2023-02-14 21:27:47 +0100

    fix tab key

--- nvim/init.lua
+++ nvim/init.lua
@@ -450,8 +450,6 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
         fallback()
       end
@@ -462,8 +460,6 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
         fallback()
       end
@@ -522,7 +518,7 @@ end, { desc = "DAP-UI: Eval expression" })
 
 require('noirbuddy').setup {
   colors = {
-    primary = '#84cc16',
+    primary = '#00ffdd',
     background = '#000000',
   },
 }
